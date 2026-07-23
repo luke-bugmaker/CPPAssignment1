17a36fcdf581c289684458a055b18a214475d162
Luke <ludaihan630@gmail.com>
2026-07-23 16:32:00 +0800

    new codes

--- test2.cpp
+++ test2.cpp
@@ -1,15 +1,13 @@
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-void grid(int ** grid, int width, int height) {
-    // upper border
+for (int y = height -1; y >= 0; y--) {
+    cout << setw(2) << y;
     for (int x = -1; x <= width; x++) {
-        cout << "# ";
+        if (x == -1 || x == width) {
+            cout << "#";
+        } else if (grid[x][y] == 0) {
+            cout << " ";
+        } else {
+        cout << grid[x][y];
+    }
     }
     cout << endl;
-
-    // middle rows
-    for (int y = )
 }
